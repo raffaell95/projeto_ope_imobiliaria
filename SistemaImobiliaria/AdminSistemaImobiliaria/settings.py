ccc832ea90751a6356965ab3ad26004e31586ae0
raffaell95 <raffaell95@live.com>
2018-10-16 01:11:10 -0300

    deploy 07

--- SistemaImobiliaria/AdminSistemaImobiliaria/settings.py
+++ SistemaImobiliaria/AdminSistemaImobiliaria/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -118,5 +120,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'http')
+
+ALLOWED_HOSTS = ['*']
+
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
