ca22fb2e5dc6bf7638a845ecce57e2d1040edb66
raffaell95 <raffaell95@live.com>
2018-11-09 11:59:12 -0200

    Conclusão do CRUD e melhoria no layout

--- SistemaImobiliaria/sistema/urls.py
+++ SistemaImobiliaria/sistema/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from sistema.views import homeSistema, cadastro, incluirCliente, incluirUsuario, incluirImovel
+from sistema.views import *
 
 urlpatterns = [
     path('', homeSistema, name='home-sistema'),
     path('cadastro/',cadastro, name='cadastro'),
     path('cadastro/incluir-clientes/',incluirCliente, name='cadastro-cliente'),
     path('cadastro/incluir-usuario/',incluirUsuario, name='cadastro-usuario'),
-    path('cadastro/incluir-imovel/',incluirImovel, name='cadastro-imovel')
+    path('cadastro/incluir-imovel/',incluirImovel, name='cadastro-imovel'),
+    path('cadastro/alterar-cliente/<int:id>/', alterarCliente, name="alterar-cliente"),
+    path('cadastro/alterar-imovel/<int:id>/', alterarImovel, name="alterar-imovel"),
+    path('cadastro/alterar-usuario/<int:id>/', alterarUsuario, name='alterar-usuario'),
+    path('cadastro/remover-cliente/<int:id>/', removerCliente, name='remover-cliente'),
+    path('cadastro/remover-imovel/<int:id>/', removerImovel, name='remover-imovel'),
+    path('cadastro/remover-usuario/<int:id>/', removerUsuario, name='remover-cliente')
 ]
--- SistemaImobiliaria/sistema/views.py
+++ SistemaImobiliaria/sistema/views.py
@@ -3,31 +3,35 @@ from sistema.models import Clientes, Imoveis
 from login.models import Usuario
 
 # Create your views here.
+
+
 def homeSistema(request):
-    
+
     return render(request, 'home-sistema.html')
+
 
 def cadastro(request):
     contexto = {
             'clientes': Clientes.objects.all(),
-            'imoveis' : Imoveis.objects.all(),
+            'imoveis': Imoveis.objects.all(),
             'usuarios': Usuario.objects.all()
     }
     return render(request, 'cadastro.html', contexto)
+
 
 def incluirCliente(request):
     if request.POST:
 
         Clientes.objects.create(
-            nome = request.POST.get('nome'),
-            idade = request.POST.get('idade'),
-            celular = request.POST.get('celular'),
-            email = request.POST.get('email'),
-            cep = request.POST.get('cep'),
-            endereco = request.POST.get('endereco'),
-            rg = request.POST.get('rg'),
-            cpf = request.POST.get('cpf'),
-            descricao = request.POST.get('descricao'),
+            nome=request.POST.get('nome'),
+            idade=request.POST.get('idade'),
+            celular=request.POST.get('celular'),
+            email=request.POST.get('email'),
+            cep=request.POST.get('cep'),
+            endereco=request.POST.get('endereco'),
+            rg=request.POST.get('rg'),
+            cpf=request.POST.get('cpf'),
+            descricao=request.POST.get('descricao'),
         )
         return redirect('/sistema/cadastro/')
 
@@ -36,10 +40,10 @@ def incluirUsuario(request):
 
     if request.POST:
         Usuario.objects.create(
-        nome = request.POST.get('nome'),
-        email = request.POST.get('email'),
-        senha = request.POST.get('senha'),
-        
+        nome=request.POST.get('nome'),
+        email=request.POST.get('email'),
+        senha=request.POST.get('senha'),
+
         )
         return redirect('/sistema/cadastro/')
 
@@ -48,14 +52,124 @@ def incluirImovel(request):
 
     if request.POST:
         Imoveis.objects.create(
-        nome_responsavel = request.POST.get('nome_responsavel'),
-        celular_responsavel = request.POST.get('celular_responsavel'),
-        endereco = request.POST.get('endereco'),
-        cep = request.POST.get('cep'),
-        valor_compra = request.POST.get('valor_compra'),
-        valor_aluguel = request.POST.get('valor_aluguel'),
-        descricao_imovel = request.POST.get('descricao_imovel')
+        nome_responsavel=request.POST.get('nome_responsavel'),
+        celular_responsavel=request.POST.get('celular_responsavel'),
+        endereco=request.POST.get('endereco'),
+        cep=request.POST.get('cep'),
+        valor_compra=request.POST.get('valor_compra'),
+        valor_aluguel=request.POST.get('valor_aluguel'),
+        descricao_imovel=request.POST.get('descricao_imovel')
         )
         return redirect('/sistema/cadastro/')
 
-                
+
+def alterarCliente(request, id):
+
+        contexto = {
+                'cliente': Clientes.objects.get(id=id)
+        }
+
+        if request.POST:
+
+                cliente = Clientes.objects.get(id=id)
+
+                cliente.nome = request.POST.get('nome')
+                cliente.idade = request.POST.get('idade')
+                cliente.celular = request.POST.get('celular')
+                cliente.email = request.POST.get('email')
+                cliente.cep = request.POST.get('cep')
+                cliente.endereco = request.POST.get('endereco')
+                cliente.rg = request.POST.get('rg')
+                cliente.cpf = request.POST.get('cpf')
+                cliente.descricao = request.POST.get('descricao')
+
+                cliente.save()
+
+                return redirect('/sistema/cadastro/')
+
+        return render(request, 'alterar-cliente.html', contexto)
+
+
+def alterarImovel(request, id):
+
+        contexto = {
+                'imovel': Imoveis.objects.get(id=id)
+        }
+
+        if request.POST:
+
+                imovel = Imoveis.objects.get(id=id)
+                imovel.nome_responsavel = request.POST.get('nome_responsavel')
+              
+                imovel.celular_responsavel = request.POST.get('celular_responsavel')
+                imovel.endereco = request.POST.get('endereco')
+                imovel.cep = request.POST.get('cep')
+                imovel.valor_compra = request.POST.get('valor_compra')
+                imovel.valor_aluguel = request.POST.get('valor_aluguel')
+                imovel.descricao_imovel = request.POST.get('descricao_imovel')
+                        
+                imovel.save()
+
+                return redirect('/sistema/cadastro/')
+
+        return render(request, 'alterar-imovel.html', contexto)
+
+
+def alterarUsuario(request, id):
+
+        contexto = {
+                'usuario': Usuario.objects.get(id=id)
+        }
+
+        if request.POST:
+
+                usuario = Usuario.objects.get(id=id)
+                usuario.nome = request.POST.get('nome')
+                usuario.email = request.POST.get('email')
+                usuario.senha = request.POST.get('senha')
+        
+                usuario.save()
+
+                return redirect('/sistema/cadastro/')
+
+        return render(request, 'alterar-usuario.html', contexto)
+
+
+def removerCliente(request, id):
+    contexto = {
+        'cliente': Clientes.objects.all(),
+    }
+    
+    if request.method == 'GET':
+        a = Clientes.objects.filter(id=id).delete()
+  
+        return redirect('/sistema/cadastro/')
+
+    return render(request, 'home.html', contexto)
+
+
+def removerImovel(request, id):
+    contexto = {
+        'imovel': Imoveis.objects.all(),
+    }
+    
+    if request.method == 'GET':
+        a = Imoveis.objects.filter(id=id).delete()
+  
+        return redirect('/sistema/cadastro/')
+
+    return render(request, 'home.html', contexto)
+
+
+def removerUsuario(request, id):
+    contexto = {
+        'usuario': Usuario.objects.all(),
+    }
+    
+    if request.method == 'GET':
+        a = Usuario.objects.filter(id=id).delete()
+  
+        return redirect('/sistema/cadastro/')
+
+    return render(request, 'home.html', contexto)
+
