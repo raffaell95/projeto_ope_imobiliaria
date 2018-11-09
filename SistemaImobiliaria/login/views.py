c781349172157d4dd00894cca1e655f20da27b18
raffaell95 <raffaell95@live.com>
2018-11-09 14:52:20 -0200

    Conclusão do Crud e login

--- SistemaImobiliaria/login/views.py
+++ SistemaImobiliaria/login/views.py
@@ -10,9 +10,17 @@ def entrar(request):
         emailusuario = request.POST.get('inputEmail')
         senhausuario = request.POST.get('inputPassword')
 
-        if Usuario.objects.get(email = emailusuario) and Usuario.objects.get(senha = senhausuario):
-                return redirect('/sistema/cadastro/')
         
+        
+        try:
+                if Usuario.objects.get(email = emailusuario) and Usuario.objects.get(senha = senhausuario):
+                        return redirect('/sistema/cadastro/')
+                else:
+                        contexto = {'erro':'senha errada'}
+                        return redirect('/')
+        except Exception as error:
+                contexto = {'erro': 'usuario nao existe'}
+                return redirect('/')
         
 
         
