333f61e6c2cd056509281f48ccdbe14dec5a955c
Jefferson <jeffersonkr@hotmail.com>
2019-04-12 14:54:23 -0300

    remocao ids models,  melhorria na views

--- SistemaImobiliaria/core/models/Boleto.py
+++ SistemaImobiliaria/core/models/Boleto.py
@@ -7,11 +7,11 @@ from .Proprietario import Proprietario
 
 
 class Boleto(models.Model):
-    id_boleto = models.IntegerField(primary_key=True)
+    #id_boleto = models.IntegerField(primary_key=True)
     id_aluguel = models.ForeignKey(Aluguel, models.DO_NOTHING, db_column='id_aluguel', blank=True, null=True)
-    id_venda = models.ForeignKey('Venda', models.DO_NOTHING, db_column='id_venda', blank=True, null=True)
-    id_cliente = models.ForeignKey('Cliente', models.DO_NOTHING, db_column='id_cliente', blank=True, null=True)
-    id_proprietario = models.ForeignKey('Proprietario', models.DO_NOTHING, db_column='id_proprietario', blank=True, null=True)
+    id_venda = models.ForeignKey(Venda, models.DO_NOTHING, db_column='id_venda', blank=True, null=True)
+    id_cliente = models.ForeignKey(Cliente, models.DO_NOTHING, db_column='id_cliente', blank=True, null=True)
+    id_proprietario = models.ForeignKey(Proprietario, models.DO_NOTHING, db_column='id_proprietario', blank=True, null=True)
     data_emissao = models.DateField(blank=True, null=True)
     data_vencimento = models.DateField(blank=True, null=True)
     valor_boleto = models.FloatField(blank=True, null=True)
